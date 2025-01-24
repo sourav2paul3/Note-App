f6bfbf9bfd0aa61e06b73420f7d59633b883da33
sourav2paul3 <40515551+sourav2paul3@users.noreply.github.com>
2025-01-24 13:05:53 +0530

    SideBar

---
+++ note-app/src/components/SideBar.tsx
@@ -0,0 +1,30 @@
+import { FaCirclePlus } from "react-icons/fa6";
+import { FaCircle } from "react-icons/fa6";
+
+const SideBar = () => {
+  const handleCreateNote = () => {
+    console.log("Create Note");
+  };
+  const handleNoteSort = () => {
+    console.log("Note Sort");
+  };
+  return (
+    <div>
+      <div
+        className=" border-r border-gray-200 w-35 h-screen flex flex-col items-center gap-15
+    py-10"
+      >
+        <button onClick={handleCreateNote} className="cursor-pointer">
+          <FaCirclePlus size={70} />
+        </button>
+        <div className="flex flex-col items-center gap-6 ">
+          <button onClick={handleNoteSort} className="cursor-pointer">
+            <FaCircle size={30} fill="red" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
