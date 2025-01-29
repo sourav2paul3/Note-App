2612e22222ce27a344ec895da1b03adda67e2b23
sourav2paul3 <40515551+sourav2paul3@users.noreply.github.com>
2025-01-30 00:20:05 +0530

    Update NoteDisplay.tsx

--- note-app/src/components/NoteDisplay.tsx
+++ note-app/src/components/NoteDisplay.tsx
@@ -47,42 +47,44 @@ const NoteDisplay: React.FC<{ note: NotesType }> = ({ note }) => {
   };
 
   return (
-    <div
-      style={{ backgroundColor: note.colour }}
-      className="w-[150px] h-[200px] md:h-[450px] md:w-[350px] rounded-lg "
-    >
-      <div className="custom-scrollbar h-[85%] border-b border-gray-600 overflow-y-auto max-h-[85%]">
-        {note.pin && (
-          <div className="flex justify-end p-1">
-            <TiPin />
+    <div>
+      <p className="font-bold text-sm">
+        {new Date(note.date).toLocaleString()}
+      </p>
+      <div
+        style={{ backgroundColor: note.colour }}
+        className="w-[150px] h-[220px] md:h-[450px] md:w-[350px] rounded-lg "
+      >
+        <div className="custom-scrollbar h-[85%] border-b border-gray-600 overflow-y-auto max-h-[85%]">
+          {note.pin && (
+            <div className="flex justify-end p-1">
+              <TiPin />
+            </div>
+          )}
+          <div className="ml-2 text-xl font-semibold ">
+            <p className="break-words">{note.note}</p>
           </div>
-        )}
-        <div className="ml-2 text-xl font-semibold ">
-          <p className="break-words">{note.note}</p>
         </div>
-      </div>
-      <div className="flex justify-between items-center gap-4 py-2">
-        <span>
-          <button className="px-2 cursor-pointer" onClick={handlePin}>
-            <TiPin size={20} fill={note.pin ? "green" : "gray"} />
+        <div className="flex justify-between items-center gap-4 py-2">
+          <span>
+            <button className="px-2 cursor-pointer" onClick={handlePin}>
+              <TiPin size={20} fill={note.pin ? "green" : "gray"} />
+            </button>
+            <button className="px-2 cursor-pointer" onClick={handleEdit}>
+              <MdEdit size={20} />
+            </button>
+            <button
+              className="px-2 cursor-pointer"
+              onClick={handleDelete}
+              disabled={note.pin}
+            >
+              <RiDeleteBin6Line size={20} fill={note.pin ? "gray" : "black"} />
+            </button>
+          </span>
+          <button className="px-4 cursor-pointer" onClick={handleStar}>
+            <FaStar size={20} fill={note.star ? "green" : "gray"} />
           </button>
-          <button className="px-2 cursor-pointer" onClick={handleEdit}>
-            <MdEdit size={20} />
-          </button>
-          <button
-            className="px-2 cursor-pointer"
-            onClick={handleDelete}
-            disabled={note.pin}
-          >
-            <RiDeleteBin6Line size={20} fill={note.pin ? "gray" : "black"} />
-          </button>
-        </span>
-        <p className="font-bold text-sm">
-          {new Date(note.date).toLocaleString()}
-        </p>
-        <button className="px-4 cursor-pointer" onClick={handleStar}>
-          <FaStar size={20} fill={note.star ? "green" : "gray"} />
-        </button>
+        </div>
       </div>
     </div>
   );
