a1d822d1c7d9fa5a682210f40511e943b90a9624
sourav2paul3 <40515551+sourav2paul3@users.noreply.github.com>
2025-01-29 22:41:18 +0530

    deploy fix

--- note-app/vite.config.ts
+++ note-app/vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // Vite configuration for GitHub Pages deployment
 export default defineConfig({
-  base: "/Note-App/note-app/", // Make sure this matches your folder structure
+  base: "/Note-App/", // Make sure this matches your folder structure
   plugins: [react(), tailwindcss()],
 });
